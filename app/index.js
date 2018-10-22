f0954acd7885b9b33780dfb3054e302dfdda87e7
jvleon <victor@kondosoft.com>
2018-10-22 18:40:40 -0500

    making conection with login

--- app/index.js
+++ app/index.js
@@ -1,7 +1,24 @@
 import { Client } from 'dsteem'
 
+import sc2 from 'steemconnect'
+
+// init steemconnect
+let api = sc2.Initialize({
+    app: 'blog-uttab',
+    callbackURL: 'http://localhost:8080',
+    accessToken: 'access_token',
+    scope: ['vote', 'comment'],
+});
+// get login URL
+let link = api.getLoginURL();
+console.log(link)
 const client = new Client('https://api.steemit.com')
 
+let access_token = new URLSearchParams(document.location.search).get(
+    'access_token'
+);
+let username = new URLSearchParams(document.location.search).get('username');
+console.log(access_token, username)
 function fetchBlog() {
     const query = {
         tag: 'steemitblog',
